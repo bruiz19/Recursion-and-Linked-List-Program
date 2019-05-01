ff16a0b808d91b8c0374a376d3b6355275adc1f3
bruiz19 <31776936+bruiz19@users.noreply.github.com>
2019-05-01 11:13:59 -0700

    Update linkedlist.h

--- linkedlist.h
+++ linkedlist.h
@@ -2,42 +2,31 @@
 #define LINKEDLIST_H
 #include <string>
 
-using namespace std;
+struct Node {
+int data;
+Node * next;
 
-struct node {
-	int data;
-	node * next;
 };
 
-class linkedlist {
+class LinkedList {
 private:
-	node * head;
-	node * headBackup;
-	//int inline count(); // RECURSIVE
-	int count(); // RECURSIVE
-	//int inline sum(); // RECURSIVE
-	int sum(); // RECURSIVE
-	//void inline printList(node* head);
-	void printList(node* head);
-	//void inline insert(node* prev, int num);
-	void insert(node* prev, int num);
-	//void inline deleteNode(node* head);
-	void deleteNode(node* head);
-	//void inline resetHead(node* headBackup);
-	void resetHead(node* headBackup);
+Node * head;
+Node * reset;
+int count();
+int sum();
+void printList(Node * head);
+void insertNode(Node * head, int num);
+void deleteNode(Node * head);
+void resetNode(Node * reset);
 
 public:
-	//inline linkedlist();
-	linkedlist();
-	//inline ~linkedlist(); // RECURSIVE
-	~linkedlist(); // RECURSIVE
-	//void inline addToFront(int n);
-	void addToFront(int n);
-	//double inline average(); // uses sum and count functions
-	double average(); // uses sum and count functions
-	//void inline writeInorder(string & file);
-	void writeInorder(string & file);
-	//void inline writeReversed(string & file);
-	void writeReversed(string & file);
+LinkedList();
+~LinkedList();
+
+void addtoFront(int n);
+double average();
+void writeInorder(string & myFile);
+void writeReversed(string & myFile);
+
 };
-#endif // end linkedlist.h
+#endif
