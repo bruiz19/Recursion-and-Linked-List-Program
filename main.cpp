dc216866e7b76fedeb56a14fde0a8e1516b0460b
bruiz19 <31776936+bruiz19@users.noreply.github.com>
2019-05-01 10:32:02 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "linkedlist.cpp"
+
+using namespace std;
+
+void readFile(linkedlist & list, string & file) {
+	ifstream readData;
+	int number;
+
+	readData.open(file);
+	while(readData >> number) {
+	list.addToFront(number);
+	}
+readData.close();
+}
+
+int main() {
+	string file = "list.txt";
+	linkedlist newList;
+	double average = 0.0;
+
+	readFile(newList, file);
+	average = newList.average();
+	cout << "The average is: " << average << endl;
+	newList.writeInorder(file);
+	newList.writeReversed(file);
+
+	return 0;
+}
