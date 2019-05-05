115c062b96c6dc6a10eeae7b933cb60712b426c9
bruiz19 <31776936+bruiz19@users.noreply.github.com>
2019-05-04 17:07:51 -0700

    Update linkedlist.h

--- linkedlist.h
+++ linkedlist.h
@@ -1,32 +1,36 @@
+//Brandon Ruiz 890130156
+//Section 02
+
+//begin linkedlist.h
 #ifndef LINKEDLIST_H
 #define LINKEDLIST_H
+
 #include <string>
 
-struct Node {
-int data;
-Node * next;
+using namespace std;
 
+struct Node {
+    int data;
+    Node * next;
 };
 
 class LinkedList {
 private:
-Node * head;
-Node * resetHead;
-int count();
-int sum();
+    Node * head;
+    Node * resetHead;
+    int count(); // RECURSIVE
+    int sum(); // RECURSIVE
+    void insertNode(Node * prev, int num);
 void printList(Node * head);
-void insertNode(Node * prev, int num);
 void deleteNode(Node * head);
 void resetNode(Node * resetHead);
 
 public:
-LinkedList();
-~LinkedList();
-
-void addtoFront(int n);
-double average();
-void writeInorder(string & file);
-void writeReversed(string & file);
-
+    LinkedList();
+    ~LinkedList(); // RECURSIVE
+    void addToFront(int n);
+    double average(); // uses sum and count functions
+    void writeInorder(string & file);
+    void writeReversed(string & file);
 };
-#endif
+#endif // end linkedlist.h
