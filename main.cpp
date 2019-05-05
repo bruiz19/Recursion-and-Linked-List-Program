6475c3a05c07cfa1cee1f4576238b67f76bcce45
bruiz19 <31776936+bruiz19@users.noreply.github.com>
2019-05-04 17:08:23 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+//Brandon Ruiz
+//Section 02
+
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -6,26 +9,27 @@
 using namespace std;
 
 void readFile(LinkedList & list, string & file) {
-	ifstream readData;
-	int number;
+ifstream readData;
+int number;
 
-	readData.open(file);
-	while(readData >> number) {
-	list.addToFront(number);
-	}
+readData.open(file);
+while(readData >> number) {
+list.addToFront(number);
+}
+
 readData.close();
 }
 
 int main() {
-	string file = "list.txt";
-	LinkedList newList;
-	double average = 0.0;
+string file = "list.txt";
+LinkedList newList;
+double average = 0.0;
 
-	readFile(newList, file);
-	average = newList.average();
-	cout << "The average is: " << average << endl;
-	newList.writeInorder(file);
-	newList.writeReversed(file);
+readFile(newList, file);
+average = newList.average();
+cout << "The average is: " << average << endl;
+newList.writeInorder(file);
+newList.writeReversed(file);
 
-	return 0;
+return 0;
 }
